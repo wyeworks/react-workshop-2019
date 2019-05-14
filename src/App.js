024f6b71447ae456736aa023c835bb34ee9efd4a
Javier Morales <javier@wyeworks.com>
2019-05-14 13:29:03 -0300

    Layout principal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="App-container">
+      <header>
+        <a>
+          <span className="logo" />
         </a>
       </header>
+      <main>
+      </main>
     </div>
   );
 }
